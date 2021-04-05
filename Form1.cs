1ce1d69065c4738c7740df93c8b2c9d2e6a47707
Luke Gebbink <lukegebbink@gmail.com>
2021-04-05 01:08:07 -0600

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guitar_Tab_Software
+{
+    public partial class MainForm : Form
+    {
+
+        FormSettings FS;
+
+        private int tuningIndex;
+
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnAddToSong_Click(object sender, EventArgs e)
+        {
+            string TxtFromString1 = textBox1.Text;
+            string TxtFromString2 = textBox2.Text;
+            string TxtFromString3 = textBox3.Text; 
+            string TxtFromString4 = textBox4.Text; 
+            string TxtFromString5 = textBox5.Text;
+            string TxtFromString6 = textBox6.Text;
+        }
+
+        private void btnWriteSongToFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPickATrack_Click(object sender, EventArgs e)
+        {
+
+        }
+ 
+        public void changeTunings(List<string> TheTuningList)
+        {
+            lblString1.Text = TheTuningList[1];
+            lblString2.Text = TheTuningList[2];
+            lblString3.Text = TheTuningList[3];
+            lblString4.Text = TheTuningList[4];
+            lblString5.Text = TheTuningList[5];
+            lblString6.Text = TheTuningList[6];
+
+        }
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.Count==1)
+            {
+                FS = new FormSettings(this);
+                FS.Show();
+            }
+            else
+            {
+                MessageBox.Show("Song Settings Are Already Open In Another Window.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+       
+        }
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guitar_Tab_Software
+{
+
+    public partial class FormSettings : Form
+    {
+
+        MainForm MF;
+
+        List<String> stringNames;
+
+        public FormSettings(MainForm form)
+        {
+            
+
+            MF = form;
+            InitializeComponent();
+
+            string[] names = new string[] { "Standard Tuning", "E FLat Tuning", "Drop D Tuning", "Drop C Tuning", "D Standard Tuning", "Drop C# Tuning" };
+
+            cbTunings.Items.AddRange(names);
+
+        }
+
+
+        public class Song
+        {
+            private int Tempo;
+            private string songName;
+            private string Tone;
+            private string TimeSignature;
+
+            public int PTempo
+            {
+                get
+                {
+                    return Tempo;
+                }
+
+                set
+                {
+                    Tempo = value;
+                }
+            }
+            public string PSongName
+            {
+                get
+                {
+                    return songName;
+                }
+
+                set
+                {
+                   songName = value;
+                }
+            }
+            public string PTone
+            {
+                get
+                {
+                    return Tone;
+                }
+
+                set
+                {
+                    Tone = value;
+                }
+            }
+            public string PTimeSignature
+            {
+                get
+                {
+                    return TimeSignature;
+                }
+
+                set
+                {
+                    TimeSignature = value;
+                }
+            }
+
+
+            
+
+        }
+
+        public FormSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void formSettings_Load(object sender, EventArgs e)
+        {
+               PictureBox pbTabGuide = new PictureBox();
+               pbTabGuide.ImageLocation = "../SamuderaJayaMotor.png";
+               pbTabGuide.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnApplySettings_Click(object sender, EventArgs e)
+        {
+
+            //When Clicked, all of the options will be confirmed and put into the main form
+
+
+
+            // Confirm the selected tuning, and assign the string letters to the selections
+
+            string selectedTuning = cbTunings.Text;
+
+            switch (selectedTuning)
+            {
+                case "Standard Tuning":
+                    List<string> StdTuningList = new List<string>() { "E", "A", "D", "G", "B", "E" };
+                    stringNames.AddRange(StdTuningList);
+                    break;
+
+                case "E Flat Tuning":
+                    List<string> EbTuningList = new List<string>() { "Eb", "Ab", "Db", "Gb", "Bb", "Eb" };
+                    stringNames.AddRange(EbTuningList);
+                    break;
+
+                case "Drop D Tuning":
+                    List<string> DropDTuningList = new List<string>() { "D", "A", "D", "G", "B", "E" };
+                    stringNames.AddRange(DropDTuningList);
+                    break;
+                case "Drop C Tuning":
+                    List<string> DropCTuningList = new List<string>() { "C", "G", "C", "F", "A", "D" };
+                    stringNames.AddRange(DropCTuningList);
+                    break;
+                case "D Standard Tuning":
+                    List<string> DStdTuningList = new List<string>() { "D", "G", "C", "F", "A", "D" };
+                    stringNames.AddRange(DStdTuningList);
+                    break;
+                case "Drop C# Tuning":
+                    List<string> DropCSharpTuningList = new List<string>() { "C#", "G#", "C#", "F#", "A#", "D#" };
+                    stringNames.AddRange(DropCSharpTuningList);
+                    break;
+            }
+
+            MF.changeTunings(stringNames);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
